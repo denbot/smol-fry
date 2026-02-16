6d1ab7f97090c6c983568af747cb475377f2fdf6
abranch6364 <49227933+abranch6364@users.noreply.github.com>
2026-02-16 13:41:39 -0700

    Added following april tags as the autonomous command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -102,6 +102,6 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
+        return new FollowAprilTagCommand(drive);
     }
 }
